533c28ef378b5d681b96766d36e9edd9ef506c74
htkz <htkzmo@gmail.com>
2018-02-03 15:48:42 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,55 +1,60 @@
-const codeArea = 123;
 const $codeArea = $('#codeArea');
 const $styleArea = $('#styleArea');
-const $mdArea = $('#mdArea')
+let $mdArea = $('#mdArea');
 
 // interval time
 const slowTime = 5;
 const quickTime = 5;
 
-const writeCss = (prefix, css, intervalTime, callback) => {
-    let n = 0;
-    const intervalId = setInterval(() => {
-        n += 1;
-        const codeString = prefix + css.substring(0, n);
-        const codeWithHighLightTag = Prism.highlight(prefix + codeString, Prism.languages.css);
-        $codeArea.html(codeWithHighLightTag);
-        $styleArea.html(codeString);
-        $codeArea.scrollTop($codeArea[0].scrollHeight);
-        if (n >= css.length) {
-            window.clearInterval(intervalId);
-            callback();
-        }
-    }, intervalTime)
-}
-
-const writeMarkdown = ($mdArea, prefix, md, intervalTime, callback) => {
-    let m = 0;
-    const intervalId_md = setInterval(() => {
-        m += 1;
-        const textString = prefix + md.substring(0, m);
-        $mdArea.html(textString);
-        $mdArea.scrollTop(codeArea[0].scrollHeight);
-        if (m >= md.length) {
-            window.clearInterval(intervalId_md);
-            callback();
-        }
-    }, intervalTime);
-}
-
-writeCss('', textInfo, slowTime, () => {
-    writeMarkdown($mdArea, '', markdownInfo, slowTime, () => {
-        writeCss(textInfo, textInfo2, slowTime, () => {
-            // to fix the bug on mobile
-            // SyntaxError: Can't create duplicate variable that shadows a global property: 'mdArea'
-            $mdArea.contents().unwrap().wrap('<div/>');
-            const newMdArea = $('.textEditor').children('div');
-            newMdArea.addClass('markdown-body')
-            writeMarkdown(newMdArea, '', marked(markdownInfo), quickTime, () => {
-                writeCss(textInfo + textInfo2, textInfo3, slowTime, () => {
-                    console.log('Finished!');
-                })
-            })
-        })
+const writeCss = (prefix, css, intervalTime) => {
+    return new Promise((resolve, reject) => {
+        let n = 0;
+        const intervalId = setInterval(() => {
+            n += 1;
+            const codeString = prefix + css.substring(0, n);
+            const codeWithHighLightTag = Prism.highlight(prefix + codeString, Prism.languages.css);
+            $codeArea.html(codeWithHighLightTag);
+            $styleArea.html(codeString);
+            $codeArea.scrollTop($codeArea[0].scrollHeight);
+            if (n >= css.length) {
+                window.clearInterval(intervalId);
+                resolve();
+            }
+        }, intervalTime)
     })
-});
+
+}
+
+const writeMarkdown = ($mdArea, prefix, md, intervalTime) => {
+    return new Promise((resolve, reject) => {
+        let m = 0;
+        const intervalId_md = setInterval(() => {
+            m += 1;
+            const textString = prefix + md.substring(0, m);
+            $mdArea.html(textString);
+            $mdArea.scrollTop($codeArea[0].scrollHeight);
+            if (m >= md.length) {
+                window.clearInterval(intervalId_md);
+                resolve();
+            }
+        }, intervalTime);
+    })
+
+}
+
+const replacePreWithDiv = () => {
+    $mdArea.contents().unwrap().wrap('<div/>');
+    $mdArea = $('.textEditor').children('div');
+    $mdArea.addClass('markdown-body')
+}
+
+const init = async () => {
+    await writeCss('', textInfo, slowTime);
+    await writeMarkdown($mdArea, '', markdownInfo, slowTime);
+    await writeCss(textInfo, textInfo2, slowTime);
+    replacePreWithDiv();
+    await writeMarkdown($mdArea, '', marked(markdownInfo), quickTime);
+    await writeCss(textInfo + textInfo2, textInfo3, slowTime);
+}
+
+init();
